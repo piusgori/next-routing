4f101007264dab7e5cc92cb3bcbc6fe777c1251f
piusgori <piusgori@gmail.com>
2023-04-21 18:09:03 +0300

    Add getStaticPaths

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import fs from 'fs/promises';
+import Link from 'next/link';
 import path from 'path';
 
 function HomePage({ products }) {
 
     return (
       <ul>
-        {products.map(prod => <li key={prod.id}>{prod.title}</li>)}
+        {products.map(prod => <li key={prod.id}><Link href={`/${prod.id}`}>{prod.title}</Link></li>)}
       </ul>
     );
   }
@@ -14,7 +15,7 @@ function HomePage({ products }) {
     const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
     const jsonData = await fs.readFile(filePath);
     const data = JSON.parse(jsonData);
-    return { props: { products: data.products } }
+    return { props: { products: data.products }, revalidate: 100 }
   }
   
   export default HomePage;
