ebe2db9e95aea5f4751fdc9d10ecf6767e7435c4
piusgori <piusgori@gmail.com>
2023-04-27 18:04:23 +0300

    Start getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import { getFeaturedEvents } from '../../dummy-data'
+import { getFeaturedEvents } from '../helpers/api-util'
 import EventList from '@/components/events/event-list';
 
-const HomePage = () => {
-
-    const featuredEvents = getFeaturedEvents();
+const HomePage = ({ events }) => {
 
   return (
     <div>
-        <EventList items={featuredEvents} />
+        <EventList items={events} />
     </div>
   )
 }
 
-export default HomePage
+export const getStaticProps = async () => {
+  const featuredEvents = await getFeaturedEvents();
+  return { props: { events: featuredEvents } };
+}
+
+export default HomePage;
