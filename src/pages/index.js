fce27f6045d66c72b78b27efdaff24f2d6e2bbbe
piusgori <piusgori@gmail.com>
2023-04-29 22:50:34 +0300

    Complete API routing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 const HomePage = () => {
+
+  const [feedbackItems, setFeedbackItems] = useState([]);
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+    fetch('/api/feedback', { 
+      method: 'POST', 
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: enteredEmail, text: enteredFeedback })
+    }).then(response => {
+      return response.json();
+    }).then(resData => {
+      console.log(resData)
+    }).catch(err => {
+      console.log(err);
+    });
+  };
+
+  const loadFeedbackHandler = () => {
+    fetch('/api/feedback').then(response => {
+      return response.json();
+    }).then(resData => {
+      setFeedbackItems(resData.feedback)
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
 
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor='email'>Your Email Address</label>
+          <input ref={emailInputRef} type='email' id='email' />
+        </div>
+        <div>
+          <label htmlFor='feedback'>Your Feedback</label>
+          <textarea ref={feedbackInputRef} rows='5' id='feedtback' />
+        </div>
+        <button type='submit'>Send Feedback</button>
+      </form>
+      <hr />
+      <button onClick={loadFeedbackHandler}>Load feedback</button>
+      <ul>
+        {feedbackItems.map(item => <li key={item.id}>{item.text}</li>)}
+      </ul>
     </div>
   )
 }
