fb0b0518f5dba7d3c4b4ee68b210c4bd56cd75e5
piusgori <piusgori@gmail.com>
2023-04-29 12:04:09 +0300

    Initialize branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,12 @@
-import React from 'react'
-import { getFeaturedEvents } from '../helpers/api-util'
-import EventList from '@/components/events/event-list';
-import Head from 'next/head';
+import React from 'react';
 
-const HomePage = ({ events }) => {
+const HomePage = () => {
 
   return (
     <div>
-      <Head>
-        <title>NextJS events</title>
-        <meta name='description' content='Find a lot of great events that allow you to evolve...' />
-      </Head>
-        <EventList items={events} />
+      <h1>The Home Page</h1>
     </div>
   )
-}
-
-export const getStaticProps = async () => {
-  const featuredEvents = await getFeaturedEvents();
-  return { props: { events: featuredEvents }, revalidate: 1000 };
 }
 
 export default HomePage;
