7cfa6b659f0ff199e45fc6e8092a5c9a4d53cd0d
piusgori <piusgori@gmail.com>
2023-05-02 07:35:14 +0300

    Add commenting

--- src/components/input/comments.js
+++ src/components/input/comments.js
@@ -1,11 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NotificationContext from '@/store/notification-context';
 
 function Comments(props) {
   const { eventId } = props;
+
+  const { showNotificationHandler } = useContext(NotificationContext);
 
   const [showComments, setShowComments] = useState(false);
   const [comments, steComments] = useState([]);
@@ -25,14 +28,23 @@ function Comments(props) {
   }
 
   function addCommentHandler(commentData) {
+    showNotificationHandler({ title: 'Sending Comment', message: 'Your Comment Is Currently being Stored Into A Database', status: 'pending' })
     fetch(`/api/comments/${eventId}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(commentData)
-    }).then(response => response.json()).then(data => {
-      console.log(data);
+    }).then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+      return response.json().then(data => {
+        throw new Error(data.message || 'Something went wrong')
+      });
+    }).then(data => {
+      showNotificationHandler({ title: 'Success', message: 'Your Comment Was Saved!', status: 'success' })
     }).catch(err => {
       console.log(err);
+      showNotificationHandler({ title: 'Error', message: err.message || 'Something went wrong', status: 'error' })
     })
   }
 
--- src/components/input/newsletter-registration.js
+++ src/components/input/newsletter-registration.js
@@ -1,22 +1,35 @@
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
 import classes from './newsletter-registration.module.css';
+import NotificationContext from '@/store/notification-context';
 
 function NewsletterRegistration() {
   const emailInputRef = useRef();
+  const { showNotificationHandler } = useContext(NotificationContext);
 
   function registrationHandler(event) {
     event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
 
+    showNotificationHandler({ title: 'Signing Up', message: 'Registering for newsletter', status: 'pending' })
+
     fetch('/api/newsletter', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email: enteredEmail })
-    }).then(response => response.json()).then(data => {
+    }).then(response => {
+      if (response.ok) {
+        return response.json()
+      }
+      return response.json().then(data => {
+        throw new Error(data.message || 'Something went wrong')
+      });
+    }).then(data => {
       console.log(data);
+      showNotificationHandler({ title: 'Success', message: 'Successfully registered for newsletter!', status: 'success' })
     }).catch(err => {
       console.log(err);
+      showNotificationHandler({ title: 'Error', message: err.message || 'Something went wrong', status: 'error' })
     });
   }
 
